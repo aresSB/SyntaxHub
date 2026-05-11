d20767296cd51947294ab2c4335ae898e13c3c48
ares <56049156+aresSB@users.noreply.github.com>
2026-05-11 12:06:41 -0400

    Update syntax.lua

--- syntax.lua
+++ syntax.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local GameScripts = {
     [130526979296684] = {name = "+1 Age Every Click", url = "https://raw.githubusercontent.com/aresSB/SyntaxHub/refs/heads/main/ageup.lua"},
-    [987654321] = {name = "Clicker Sim", url = "https://githubusercontent.com"}
+    [112259901434347] = {name = "Be a Lucky Block", url = "https://raw.githubusercontent.com/aresSB/SyntaxHub/refs/heads/main/luckyblock.lua"}
 }
 
 local placeId = game.PlaceId
